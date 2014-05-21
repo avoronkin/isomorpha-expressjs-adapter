e3ee25821285fc8dde88ea474025535e52595e2e
Alexandr Voronkin <admin@elasticweb.ru>
2014-05-22 00:17:16 +0400

    express-shared-routes support

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
 var _ = require('lodash');
 
-module.exports = function (routeTable, app) {
+module.exports = function (routeTable, routeManager) {
     if (!routeTable) {
-        throw new Error('express adapter: routeTable required!');
+        throw new Error('express-shared-routes adapter: routeTable required!');
     }
-    if (!app) {
-        throw new Error('express adapter: express.js instance required!');
+    if (!routeManager) {
+        throw new Error('express-shared-routes adapter: routeManager instance required!');
     }
     if (!_.isArray(routeTable)) {
-        throw new Error('express adapter: routeTable must be array!');
+        throw new Error('express-shared-routes adapter: routeTable must be array!');
     }
-    _.each(routeTable, function(route){
-        addRoute(route, app);
+    _.each(routeTable, function (route) {
+        addRoute(route, routeManager);
     });
 };
 
-function addRoute(route, app, parentPattern) {
+function addRoute(route, routeManager, parentName) {
     if (_.isUndefined(route.method)) route.method = 'get';
-    parentPattern = parentPattern || '';
 
     if (!route.pattern) {
-        throw new Error('express adapter addRoute(): route.pattern required!', route);
+        throw new Error('express-shared-routes adapter addRoute(): route.pattern required!', route);
     }
     if (!route.handlers) {
-        throw new Error('express adapter addRoute(): route.handler required!', route);
+        throw new Error('express-shared-routes adapter addRoute(): route.handler required!', route);
     }
 
     if (_.isFunction(route.handlers)) {
         route.handlers = [route.handlers];
     }
 
-    route.pattern = parentPattern + route.pattern;
-    app[route.method].apply(app,[route.pattern].concat(route.handlers));
-    parentPattern = route.pattern;
+    var routeArguments = [{
+            name: route.name,
+            re: route.pattern,
+            parent: parentName ? routeManager.getRoute(parentName) : null
+        },
+        route.handlers
+    ];
+
+    routeManager[route.method].apply(routeManager, routeArguments);
 
     if (_.isArray(route.routes)) {
-        _.each(route.routes, function (route) {
-            addRoute(route, app, parentPattern);
+        _.each(route.routes, function (r) {
+            addRoute(r, routeManager, route.name);
         });
     }
 }
-
 
